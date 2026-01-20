c81c71108d2a03a52380a380beb2b63a9bfc85fd
Pradeep2662 <pradeepyanagandula06@gmail.com>
2026-01-20 16:35:10 +0530

    Add files via upload

---
+++ L1P1.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+class L1P1{
+	int x=10;
+	
+	public static void main(String []args){
+		int a=5;
+		double t=a;
+		System.out.println(t);
+		double b=5.58;
+		int x=(int)b;
+		System.out.println(x);
+	}
+}
